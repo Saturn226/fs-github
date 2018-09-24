1ecdc360ff1aaf1155f78862086c5dcd07fbfc05
Saturn226 <serenehopes@gmail.com>
2018-09-24 13:23:51 -0400

    renderResult maps over data and adds it to the dom

--- src/index.js
+++ src/index.js
@@ -7,9 +7,20 @@ export const getRepos = (name) => {
 }
 
 export const renderResult = (data) => {
+    const {login, name, location, company, bio, 
+        email, repos_url,public_repos  } = data
+    
+    const data_array = [name, company, location,bio, 
+            email, public_repos, repos_url]
     let html = ""
-    html += `<h1>${data.login}</h1>`
+    html += `<h1>${login}</h1>`
+    data_array.map(data => {
+        if (data)
+           html += `<p>${data}</p>`
+        return html
+    })
     document.getElementById("root").innerHTML = html
+
 }
 
 export const getReposAndRender = (name) => {
