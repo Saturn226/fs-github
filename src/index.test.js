da4b58da7909a7444a656de1f1a9ea2226bcc63c
Saturn226 <serenehopes@gmail.com>
2018-09-24 10:56:05 -0400

    added test for getRepos. Expect it to be defined

--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import {getRepos} from './index.js'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('#getRepos()', () => {
+  it('is defined', () =>{
+    expect(getRepos).not.toBe(undefined)
+  })
+})
