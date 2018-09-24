99e59b2f7d3e7c534146e63ff5705baeadaf48f9
Saturn226 <serenehopes@gmail.com>
2018-09-24 11:15:17 -0400

    get repos fetches a user based on their name and returns data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-
+import fetch from "isomorphic-fetch"
 import registerServiceWorker from './registerServiceWorker';
 
 export const getRepos = (name) => {
-
+    return fetch(`http://api.github.com/users/${name}`)
+        .then(res => res.json())
 }
 
 
--- src/index.test.js
+++ src/index.test.js
@@ -4,4 +4,13 @@ describe('#getRepos()', () => {
   it('is defined', () =>{
     expect(getRepos).not.toBe(undefined)
   })
+
+//fetch returns a promise
+  it('should load user data', () => {
+    getRepos("saturn226") // my repo
+      .then(data => {
+        expect(data).toBeDefined
+        expect(data.name).toBe("Ashley")
+      })
+  })
 })
