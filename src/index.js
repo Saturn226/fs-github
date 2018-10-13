7ad84b9e3c8c680419cfd600b061039ab27e605f
Saturn226 <serenehopes@gmail.com>
2018-10-13 14:33:12 -0400

    Adds other react files

--- src/index.js
+++ src/index.js
@@ -1,38 +1,8 @@
-import fetch from "isomorphic-fetch"
+import React from 'react';	
+import ReactDOM from 'react-dom';	
+import './index.css';	
+import App from './App';	
+import registerServiceWorker from './registerServiceWorker';
 
-export const getRepos = (name) => {
-    return fetch(`http://api.github.com/users/${name}`)
-        .then(res => res.json())
-}
-
-export const renderResult = (data) => {
-    let html = ""
-    html += `<h1>User: ${data.login}</h1>
-            <h2>Name: ${data.name}</h2>`
-    Object.keys(data).forEach(key => {
-        if (data[key] && key !== 'login' && key !== 'name')
-           html += `<p>${key} : ${data[key]}</p>`
-    })
-    document.getElementById("root").innerHTML = html
-}
-
-export const getReposAndRender = (name) => {
-    getRepos(name)
-        .then(data => renderResult(data))
-}
-
-export const searchForRepos = () =>{
-    const search = document.getElementById("query").value
-    getReposAndRender(search);   
-}
-
-document.addEventListener('DOMContentLoaded', function(){
-    document.getElementById("search-box").addEventListener("submit", function(e){
-    e.preventDefault(); 
-    searchForRepos();
-    })
-})
-
-
-
-
+ReactDOM.render(<App />, document.getElementById('root'));	
+registerServiceWorker();
