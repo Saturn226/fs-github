d1e2035ddb7bf938ec986736bab772a7b3477a42
Saturn226 <serenehopes@gmail.com>
2018-10-15 03:07:40 -0400

    added additional info

--- src/components/resultPageComponent.js
+++ src/components/resultPageComponent.js
@@ -1,54 +1,58 @@
-import React, {Component} from "react";
-import styled from "styled-components"
-import ReposComponent from "./reposComponent.js"
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import ReposComponent from './reposComponent.js';
 
-export default class ResultsPageComponent extends Component {    
-    
-    
-   render(){ 
+export default class ResultsPageComponent extends Component {
+  render(){
+    if(this.props.error){
+      return <h1>{this.props.error}</h1>
+    }
     if (this.props.user) {
-        const {login, avatar_url, repos_url, name, organizations_url,bio} = this.props.user
-        
-        return (
-                <Div>
-                        <Img src={avatar_url}/>
-                        <Ul>
-                            <li><h1>{login}</h1></li>
-                            <li><h2>{name}</h2></li>
-                            <li>{bio}</li>
+      const {
+        login, avatar_url, repos_url, name = "NA", organizations_url, bio,
+      html_url, company, location, blog, id} = this.props.user
 
-                        </Ul>
+      const shamelessPlug = login === "Saturn226" ? <h1>HIRE ME!</h1> : null
+  
+      return (
+        <Div>
+          <Img src={avatar_url} />
+          <Ul>
+            <a href={html_url}><li><h1>{login || "N/A"}</h1></li></a>
+            <li><h2>{name || "N/A"}</h2></li>
 
-                        <ReposComponent repos_url={repos_url}/>
-                  
-                </Div>
-        );
-    }else
-    return(<h1>Search for A user!</h1>)
-   }
+            <li><strong>Bio:</strong>     {bio || "N/A"}</li>
+            <li><strong>Company:</strong> {company || "N/A"}</li>
+            <li><strong>Location:</strong>{location|| "N/A"}</li>
+            <li><strong>Blog:</strong>    {blog|| "N/A"}</li>
+            {shamelessPlug}
+          </Ul>
+          <ReposComponent repos_url={repos_url} />
 
-};
+        </Div>
+      );
+    } return (<h1>Search for a user!</h1>);
+  }
+}
 
 const Ul = styled.ul`
     list-style: none;
     text-align: left;
     grid-area: info;
-`
+`;
 const Div = styled.div`
   grid-area: results;
-  border: 5px solid lightpink;
+  border: 5px solid #BDF1FD;
   display: grid;
   grid-template-columns: 400px 1fr
   grid-template-areas: "img info"
                         "repos repos";
   padding: 20px;
-
-  background: white //TODO: Delete this later
-`
+`;
 
 const Img = styled.img`
     margin: 20px;
     max-width: 100%;
     grid-area: img;
 
-`
+`;
