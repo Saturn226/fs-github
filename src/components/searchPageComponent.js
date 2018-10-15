946287325c9d1c81d41c1da34200a69543331540
Saturn226 <serenehopes@gmail.com>
2018-10-15 01:32:36 -0400

    created getInitialState method to better handle state change

--- src/components/searchPageComponent.js
+++ src/components/searchPageComponent.js
@@ -3,25 +3,33 @@ import styled from "styled-components"
 import ResultsPageComponent from "./resultPageComponent.js"
 
 export class SearchPageComponent extends Component {
-    constructor() {
-        super();
-        this.state = {
-            searchUser: ""
+
+    getInitialState = () => {
+        return {
+            searchUser: "",
+            error: null
         }
     }
 
+    state = this.getInitialState();
+    
     handleErrors = (response) => {
-        if(!response.ok)
+        if(!response.ok){
             throw Error(response.statusText);
-            return response.json()
+        }
+        return response.json()
     }
+
+    
 
     searchUser = () => {
         if (this.state.searchUser) {
             return fetch(`http://api.github.com/users/` + this.state.searchUser) 
                 .then(res => this.handleErrors(res))
                 .then(user => this.setState({user}))
-                .catch((e) => {this.setState({error: e.message})})
+                .then(()=> this.setState(this.getInitialState()))
+
+                .catch((e) => this.setState({error: e.message}))
         }
         else
             this.setState({error: "Not Found"})
@@ -34,18 +42,17 @@ export class SearchPageComponent extends Component {
     handleOnClick = (e) => {
         e.preventDefault();
         this.searchUser();
-        this.setState({searchUser: ""})
+        this.setState({searchUser: ""});
     }
 
     render() {
         return (
             <Div>
                 <SearchArea>
-                    <p>Search Page Component</p>
                     <form onSubmit={this.handleOnClick}>
                         Search for user
                         <input type="text" required value={this.state.searchUser} onChange={this.handleOnChange}></input>
-                        <input type="submit"></input>
+                        <button>Submit</button>
                     </form>
                 </SearchArea>
                 <ResultsPageComponent user={this.state.user} error={this.state.error}/>
